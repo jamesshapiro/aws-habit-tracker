9ecfd0c6702b5a3e4bd8bce35787744ee627c7e4
James Shapiro <jamesshapiro@gmail.com>
2022-02-11 19:41:03 -0500

    add habit display website with SSL

--- cdk_habit_tracker/cdk_habit_tracker_stack.py
+++ cdk_habit_tracker/cdk_habit_tracker_stack.py
@@ -6,7 +6,13 @@ from aws_cdk import (
     aws_sns as sns,
     aws_sns_subscriptions as subscriptions,
     aws_iam as iam,
-    aws_apigateway as apigateway
+    aws_apigateway as apigateway,
+    aws_s3 as s3,
+    aws_cloudfront as cloudfront,
+    aws_certificatemanager as certificatemanager,
+    aws_route53 as route53,
+    aws_route53_targets as route53_targets,
+    aws_cloudfront_origins as origins,
 )
 from constructs import Construct
 
@@ -20,7 +26,13 @@ class CdkHabitTrackerStack(Stack):
             lines = f.read().splitlines()
             # .cdk-params should be of the form:
             # account_id=12345678901234
-            phone_number = [line for line in lines if line.startswith('NotificationPhone')][0].split('=')[1]
+            phone_number = [line for line in lines if line.startswith('phone_number')][0].split('=')[1]
+            email = [line for line in lines if line.startswith('email')][0].split('=')[1]
+            subdomain_name = [line for line in lines if line.startswith('subdomain_name')][0].split('=')[1]
+            hosted_zone_id = [line for line in lines if line.startswith('hosted_zone_id')][0].split('=')[1]
+            zone_name = [line for line in lines if line.startswith('zone_name')][0].split('=')[1]
+        
+        # HABIT TRACKER BACK-END
         ddb_table = dynamodb.Table(
             self, 'Table',
             partition_key=dynamodb.Attribute(name='PK1', type=dynamodb.AttributeType.STRING),
@@ -230,4 +242,37 @@ class CdkHabitTrackerStack(Stack):
                     response_parameters={'method.response.header.Access-Control-Allow-Origin': True}
                 )
             ]
+        )
+
+        # HABIT TRACKER WEBSITE
+        site_bucket = s3.Bucket(
+            self, 'bucket',
+        )
+        zone = route53.HostedZone.from_hosted_zone_attributes(self, "HostedZone",
+            hosted_zone_id=hosted_zone_id,
+            zone_name=zone_name
+        )
+        certificate = certificatemanager.DnsValidatedCertificate(
+            self, 'certificate',
+            domain_name=subdomain_name,
+            hosted_zone=zone
+        )
+        distribution = cloudfront.Distribution(
+            self, 'distribution',
+            default_behavior=cloudfront.BehaviorOptions(
+                origin=origins.S3Origin(site_bucket),
+                allowed_methods=cloudfront.AllowedMethods.ALLOW_ALL,
+                viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS
+            ),
+            comment='S3 HTTPS example',
+            default_root_object='index.html',
+            domain_names=[subdomain_name],
+            certificate=certificate
+        )
+        a_record_target = route53.RecordTarget.from_alias(route53_targets.CloudFrontTarget(distribution))
+        route53.ARecord(
+            self, 'alias-record',
+            zone=zone,
+            target=a_record_target,
+            record_name=subdomain_name
         )
