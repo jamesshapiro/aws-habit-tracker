0d5285a10fb79524ea9ac7fe93668d94eada2cda
James Shapiro <jamesshapiro@gmail.com>
2022-02-13 08:56:00 -0500

    add default blank data cells to fill in for habits with missing entries (i.e. deleted and the later re-added habits)

--- my-app/src/components/Habit/Habit.js
+++ my-app/src/components/Habit/Habit.js
@@ -9,6 +9,37 @@ class Habit extends Component {
       isMounted: false,
     }
   }
+
+  getDaysOfYear = () => {
+    const today = new Date()
+    const tomorrow = new Date(today)
+    tomorrow.setDate(tomorrow.getDate() + 1)
+    const dd = String(tomorrow.getDate()).padStart(2, '0')
+    const mm = String(tomorrow.getMonth() + 1).padStart(2, '0')
+    const yyyy = tomorrow.getFullYear()
+    const day = today.getDay()
+    const numDaysToFetch =
+      Math.min(window.screen.width, window.innerWidth) > 650
+        ? 365 + day
+        : 120 + day
+    var daysOfYear = []
+    for (
+      var d = new Date(yyyy - 2, mm, dd);
+      d <= today;
+      d.setDate(d.getDate() + 1)
+    ) {
+      daysOfYear.push({
+        count: 0,
+        date: `${d.getFullYear()}-${String(d.getMonth() + 1).padStart(
+          2,
+          '0'
+        )}-${String(d.getDate()).padStart(2, '0')}`,
+        level: 0
+      })
+    }
+    return daysOfYear.slice(-numDaysToFetch)
+  }
+
   getNewEntries = () => {
     const today = new Date()
     const tomorrow = new Date(today)
@@ -23,23 +54,33 @@ class Habit extends Component {
     var url =
       process.env.REACT_APP_GET_HABIT_DATA_URL +
       `?user=${james}&PK1=${this.props.habitName}&limit=${numDaysToFetch}&startkey=${yyyymmdd}`
-    for (let i = 0; i < numDaysToFetch; i++) {
-
-    }
+    var daysOfYear = this.getDaysOfYear()
     fetch(url, {
       method: 'GET',
     })
       .then((response) => response.json())
       .then((data) => {
-        const dataItems = data.Items.map((item) => (
-           {
+        const dataItems = data.Items.map((item) => {
+          return {
             count: parseInt(item.DATE_COUNT.S),
             date: item.SK1.S.slice(5),
             level: parseInt(item.DATE_LEVEL.S),
           }
-      ))
-        dataItems.reverse()
-        const newState = { dataPoints: [...dataItems], isMounted: true }
+        }
+          
+          
+      )
+        //dataItems.reverse()
+        dataItems.map((item) => {
+          daysOfYear.forEach((dayOfYear) => {
+            if (item.date === dayOfYear.date) {
+              dayOfYear.count = item.count
+              dayOfYear.level = item.level
+            }
+          })
+        })
+        const newState = { dataPoints: [...daysOfYear], isMounted: true }
+        //const newState = { dataPoints: [...dataItems], isMounted: true }
         this.setState(newState)
       })
   }
@@ -58,6 +99,7 @@ class Habit extends Component {
           {this.state.isMounted && (
             <ActivityCalendar
               color={this.props.habitColor}
+              //data = {this.getDaysOfYear()}
               data={this.state.dataPoints}
               hideColorLegend={false}
               hideTotalCount={true}
