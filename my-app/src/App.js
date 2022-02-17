b94d6f11ee8ca08f5ee9ff715af8c07cd78f9703
James Shapiro <jamesshapiro@gmail.com>
2022-02-16 21:44:29 -0500

    standard spacing across screen sizes, add sign-out button and functionality, refresh page on sign-in/sign-out, fetch user specific habit data based on retrieved email, add mock data for default user display using a deterministic hash of the habit name + the date, add login as a separate page, add button to test session functionality

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,5 @@
 //aws cloudfront create-invalidation --distribution-id E70XD704NPJDM --paths "/*"
 //aws s3 cp --recursive build/ s3://cdkhabits-habitsweakerpotionscombucketdff06391-116yh481gtpp6
-import logo from './logo.svg';
 import './App.css';
 import React from 'react'
 import Habit from './components/Habit/Habit'
@@ -12,64 +11,208 @@ import '@aws-amplify/ui-react/styles.css'
 import awsExports from './aws-exports'
 Amplify.configure(awsExports)
 
+
+async function signOut() {
+  try {
+    await Auth.signOut({ global: true })
+    window.location.reload(false)
+  } catch (error) {
+    console.log('error signing out: ', error)
+  }
+}
+
+async function currentSession() {
+  try {
+    const result = await Auth.currentSession()
+    return result
+  } catch (error) {
+    console.log('error retrieving current session: ', error)
+  }
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       habits: [],
-      isMounted: false
+      isMounted: false,
+      isLoginPage: false,
     }
   }
 
+  // TODO: try this with functional components later
+  // getCurrentUser = async () => {
+  //   try {
+  //     const user = await Auth.currentAuthenticatedUser()
+  //     return user.attributes.email
+  //   } catch (error) {
+  //     return 'display'
+  //   }
+  // }
+
+  // colors = ['#b92514', '#2270A1', '#1e4500']
+
+  getEntriesForUser = (user) => {
+    
+  }
+
   getNewEntries = () => {
-    var default_user = "display"
-    var url =
-      process.env.REACT_APP_GET_HABITS_URL + `?user=${default_user}`
-    fetch(url, {
-      method: 'GET',
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        const habitItems = data.Items.map((item) => {
-          return {
-            habitName: item.SK1.S.slice(6),
-            habitColor: item.COLOR.S,
-            habitPriority: item.hasOwnProperty('PRIORITY') ? parseInt(item.PRIORITY.S) : 0
-          }
+    Auth.currentAuthenticatedUser()
+      .then((user) => {
+        const username = user.attributes.email.replace('@', '%40')
+        var url = process.env.REACT_APP_GET_HABITS_URL + `?user=${username}`
+        fetch(url, {
+          method: 'GET',
         })
-        habitItems.sort(
-          (habit1, habit2) => habit2.habitPriority - habit1.habitPriority
-        )
-        var newState = { habits: [...habitItems], isMounted: true }
-        this.setState(newState)
+          .then((response) => response.json())
+          .then((data) => {
+            const habitItems = data.Items.map((item) => {
+              return {
+                habitName: item.SK1.S.slice(6),
+                habitColor: item.COLOR.S,
+                habitPriority: item.hasOwnProperty('PRIORITY')
+                  ? parseInt(item.PRIORITY.S)
+                  : 0,
+              }
+            })
+            habitItems.sort(
+              (habit1, habit2) => habit2.habitPriority - habit1.habitPriority
+            )
+            var newState = {
+              habits: [...habitItems],
+              isMounted: true,
+              isLoginPage: false,
+            }
+            this.setState(newState)
+          })
+        this.getEntriesForUser(username)
       })
+      .catch((err) => {
+        const habits = [
+          {
+            habitName: 'clean-for-10m',
+            habitColor: '#b92514',
+            habitPriority: 100,
+          },
+          {
+            habitName: 'get-8h-of-sleep',
+            habitColor: '#2270A1',
+            habitPriority: 99,
+          },
+          {
+            habitName: 'stay-hydrated',
+            habitColor: '#1e4500',
+            habitPriority: 98,
+          },
+          {
+            habitName: 'read-for-30m',
+            habitColor: '#b92514',
+            habitPriority: 97,
+          },
+          {
+            habitName: 'exercise',
+            habitColor: '#2270A1',
+            habitPriority: 96,
+          },
+        ]
+        var newState = {
+          habits: habits,
+          isMounted: true,
+          isLoginPage: false,
+        }
+        this.setState(newState)
+        return
+      })
+    // var url = process.env.REACT_APP_GET_HABITS_URL + `?user=${user}`
+    // fetch(url, {
+    //   method: 'GET',
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     const habitItems = data.Items.map((item) => {
+    //       return {
+    //         habitName: item.SK1.S.slice(6),
+    //         habitColor: item.COLOR.S,
+    //         habitPriority: item.hasOwnProperty('PRIORITY')
+    //           ? parseInt(item.PRIORITY.S)
+    //           : 0,
+    //       }
+    //     })
+    //     habitItems.sort(
+    //       (habit1, habit2) => habit2.habitPriority - habit1.habitPriority
+    //     )
+    //     var newState = {
+    //       habits: [...habitItems],
+    //       isMounted: true,
+    //       isLoginPage: false,
+    //     }
+    //     this.setState(newState)
+    //   })
   }
 
   getHabitGraphs = () => {
-    return <div className="habit-graphs">
-      {this.state.habits.map(habit => {
-        return <Habit habitName={habit.habitName} habitColor={habit.habitColor} />
-      })}
-    </div>
+    return (
+      <div className="habit-graphs">
+        {this.state.habits.map((habit) => {
+          return (
+            <Habit habitName={habit.habitName} habitColor={habit.habitColor} />
+          )
+        })}
+      </div>
+    )
   }
 
   componentDidMount() {
     this.getNewEntries()
   }
 
-  render() {
+  goToLogin = () => {
+    let newState = {
+      isLoginPage: true,
+    }
+    this.setState(newState)
+  }
+
+  getUserSession = () => {
+    const result = currentSession()
+    console.log(result)
+  }
+
+  getGraphsForUser = () => {
     return (
       <div>
         <div className="App">
-          <h1 className="habit-h1">Habit Tracker</h1>
-          {/* <button onClick={signOut}>Sign out</button> */}
+          <div className="habit-h1">
+            Habit Tracker
+            <button onClick={this.goToLogin}>Login/Signup</button>
+            <button onClick={() => signOut()}>Signout</button>
+            <button onClick={() => currentSession()}>SESSION</button>
+          </div>
+          <h1 className="habit-h1">{}</h1>
           {this.state.isMounted && this.getHabitGraphs()}
         </div>
-        <Authenticator>
-          {({ signOut, user }) => (<p>Hello World!</p>)}
-        </Authenticator>
       </div>
     )
+  }
+
+  getFrontPage = () => {
+    if (this.state.isLoginPage) {
+      return (
+        <Authenticator>
+          {({ signOut, user }) => {
+            this.setState({ isLoginPage: false })
+            window.location.reload(false)
+            return this.getGraphsForUser()
+          }}
+        </Authenticator>
+      )
+    } else {
+      return this.getGraphsForUser()
+    }
+  }
+
+  render() {
+    return this.getFrontPage()
   }
 }
 
--- my-app/src/components/Habit/Habit.js
+++ my-app/src/components/Habit/Habit.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import ActivityCalendar from 'react-activity-calendar'
+import { Amplify, Auth } from 'aws-amplify'
 
+import sha256 from 'crypto-js/sha256'
+import Base64 from 'crypto-js/enc-base64'
 
 const SCREEN_WIDTH = 850
 
@@ -22,7 +25,27 @@ class Habit extends Component {
     return numDaysToFetch
   }
 
-  getDaysOfYear = () => {
+  getRandomDigitFromDateString = (dateString, habitName, user) => {
+    if (user !== 'display') {
+      return 0
+    }
+    const hashDigest = sha256(habitName + dateString)
+    const lastDigit = Math.floor(Math.abs(hashDigest.words.pop() % 10) / 2)
+    let secondToLastDigit = 0
+    if (lastDigit === 0) {
+      const otherDigit = Math.floor(
+        Math.floor(Math.abs(hashDigest.words.pop() % 10) / 2)
+      )
+      if (otherDigit <= 2) {
+        secondToLastDigit = otherDigit
+      }
+    }
+    
+    const result = Math.min(lastDigit + secondToLastDigit, 4)
+    return result
+  }
+
+  getDaysOfYear = (user, habitName) => {
     const today = new Date()
     const tomorrow = new Date(today)
     tomorrow.setDate(tomorrow.getDate() + 1)
@@ -30,24 +53,43 @@ class Habit extends Component {
     const mm = String(tomorrow.getMonth() + 1).padStart(2, '0')
     const yyyy = tomorrow.getFullYear()
     var daysOfYear = []
+
     for (
       var d = new Date(yyyy - 2, mm, dd);
       d <= today;
       d.setDate(d.getDate() + 1)
     ) {
+      let dateCount = this.getRandomDigitFromDateString(
+        `${d.getFullYear()}-${String(d.getMonth() + 1).padStart(
+          2,
+          '0'
+        )}-${String(d.getDate()).padStart(2, '0')}`,
+        habitName,
+        user
+      )
       daysOfYear.push({
-        count: 0,
+        count: dateCount,
         date: `${d.getFullYear()}-${String(d.getMonth() + 1).padStart(
           2,
           '0'
         )}-${String(d.getDate()).padStart(2, '0')}`,
-        level: 0,
+        level: dateCount,
       })
     }
     return daysOfYear
   }
 
-  getNewEntries = () => {
+  // TODO: try this again with functional components
+  // getCurrentUser = async () => {
+  //   try {
+  //     const user = await Auth.currentAuthenticatedUser()
+  //     return user.attributes.email
+  //   } catch (error) {
+  //     return 'display'
+  //   }
+  // }
+
+  getSquaresForUser = (user) => {
     const tomorrow = new Date()
     tomorrow.setDate(tomorrow.getDate() + 1)
     const dd = String(tomorrow.getDate()).padStart(2, '0')
@@ -55,35 +97,79 @@ class Habit extends Component {
     const yyyy = tomorrow.getFullYear()
     const yyyymmdd = `${yyyy}-${mm}-${dd}`
     const numDaysToFetchFromDDB = 373
-    var default_user = 'display'
-    //var default_user = 'user@example.com'.replace('@', '%40')
     var url =
       process.env.REACT_APP_GET_HABIT_DATA_URL +
-      `?user=${default_user}&PK1=${this.props.habitName}&limit=${numDaysToFetchFromDDB}&startkey=${yyyymmdd}`
-    var daysOfYear = this.getDaysOfYear()
-    fetch(url, {
-      method: 'GET',
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        const dataItems = data.Items.map((item) => {
-          return {
-            count: parseInt(item.DATE_COUNT.S),
-            date: item.SK1.S.slice(5),
-            level: Math.min(parseInt(item.DATE_LEVEL.S), 4),
-          }
-        })
-        dataItems.map((item) => {
-          daysOfYear.forEach((dayOfYear) => {
-            if (item.date === dayOfYear.date) {
-              dayOfYear.count = item.count
-              dayOfYear.level = item.level
+      `?user=${user}&PK1=${this.props.habitName}&limit=${numDaysToFetchFromDDB}&startkey=${yyyymmdd}`
+    var daysOfYear = this.getDaysOfYear(user, this.props.habitName)
+    if (user != 'display') {
+      fetch(url, {
+        method: 'GET',
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          const dataItems = data.Items.map((item) => {
+            return {
+              count: parseInt(item.DATE_COUNT.S),
+              date: item.SK1.S.slice(5),
+              level: Math.min(parseInt(item.DATE_LEVEL.S), 4),
             }
           })
+          dataItems.map((item) => {
+            daysOfYear.forEach((dayOfYear) => {
+              if (item.date === dayOfYear.date) {
+                dayOfYear.count = item.count
+                dayOfYear.level = item.level
+              }
+            })
+          })
+          const newState = { dataPoints: [...daysOfYear], isMounted: true }
+          this.setState(newState)
         })
-        const newState = { dataPoints: [...daysOfYear], isMounted: true }
-        this.setState(newState)
+    } else {
+      const newState = { dataPoints: [...daysOfYear], isMounted: true }
+      this.setState(newState)
+    }
+  }
+
+  getNewEntries = () => {
+    // let user = this.getCurrentUser()
+    Auth.currentAuthenticatedUser()
+      .then((user) => {
+        let username = user.attributes.email.replace('@', '%40')
+        this.getSquaresForUser(username)
       })
+      .catch((err) => {
+        let username = 'display'
+        this.getSquaresForUser(username)
+      })
+    //var default_user = 'user@example.com'.replace('@', '%40')
+    // var url =
+    //   process.env.REACT_APP_GET_HABIT_DATA_URL +
+    //   `?user=${user}&PK1=${this.props.habitName}&limit=${numDaysToFetchFromDDB}&startkey=${yyyymmdd}`
+    // var daysOfYear = this.getDaysOfYear()
+    // fetch(url, {
+    //   method: 'GET',
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     const dataItems = data.Items.map((item) => {
+    //       return {
+    //         count: parseInt(item.DATE_COUNT.S),
+    //         date: item.SK1.S.slice(5),
+    //         level: Math.min(parseInt(item.DATE_LEVEL.S), 4),
+    //       }
+    //     })
+    //     dataItems.map((item) => {
+    //       daysOfYear.forEach((dayOfYear) => {
+    //         if (item.date === dayOfYear.date) {
+    //           dayOfYear.count = item.count
+    //           dayOfYear.level = item.level
+    //         }
+    //       })
+    //     })
+    //     const newState = { dataPoints: [...daysOfYear], isMounted: true }
+    //     this.setState(newState)
+    //   })
   }
 
   resize = () => this.forceUpdate()
@@ -95,7 +181,7 @@ class Habit extends Component {
 
   getTitle = (habitName) =>
     habitName.charAt(0).toUpperCase() + habitName.replaceAll('-', ' ').slice(1)
-  
+
   render() {
     const numDaysToFetch = this.getNumDaysToFetch()
     const mytheme = this.props.habitName.toLowerCase().includes('github')
